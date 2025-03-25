f87ea73a0ab97fd20851fadc69207c1476ea30be
JWN <naa3000@naver.com>
2025-03-25 22:46:01 +0900

    natural stop

--- src/client/java/jwn/auto_run/mixin/client/ClientPlayerEntityMixin.java
+++ src/client/java/jwn/auto_run/mixin/client/ClientPlayerEntityMixin.java
@@ -4,6 +4,7 @@ import jwn.auto_run.IClientPlayerAccess;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
@@ -24,7 +25,12 @@ public class ClientPlayerEntityMixin implements IClientPlayerAccess {
 
         if (!this.autoRunMode) {
             MinecraftClient.getInstance().player.setSprinting(false);
-            MinecraftClient.getInstance().options.forwardKey.setPressed(false);
+
+            KeyBindingAccessor forwardKey = (KeyBindingAccessor) MinecraftClient.getInstance().options.forwardKey;
+            long window = MinecraftClient.getInstance().getWindow().getHandle();
+            if (!(GLFW.glfwGetKey(window, forwardKey.getBoundKey().getCode()) == GLFW.GLFW_PRESS)) {
+                MinecraftClient.getInstance().options.forwardKey.setPressed(false);
+            }
         } else {
             MinecraftClient.getInstance().player.setSprinting(true);
         }
